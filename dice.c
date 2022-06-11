c17554d4640325f66148a3d192eb8f67052960ca
wu cheng <72378528+zerk895@users.noreply.github.com>
2022-06-12 01:38:34 +0800

    Update dice.c

--- dice.c
+++ dice.c
@@ -1,1 +1,21 @@
+#include<main.c>//i'm not sure 
 
+
+void medecine(struct people **start, const char *ppl_name)
+{
+    
+    struct people *cur = *start;
+    if (strcmp(cur->city, ppl_name))
+    {
+        cur = cur->next;
+    }
+    else
+    {
+        srand( time(NULL) );
+        int min = 1;
+        int max = 3;
+        int x = rand() % (max - min + 1) + min;
+        if(x==3)
+            cur->remain_day = 0;
+    }
+}
